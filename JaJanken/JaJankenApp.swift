3135faff0d9ac3de797739a9d2cf3243e33e66b8
Carlos Mbendera <carlosmbendera@gmail.com>
2023-04-14 20:57:29 -0500

    Initial Commit

---
+++ JaJanken/JaJankenApp.swift
@@ -0,0 +1,17 @@
+//
+//  JaJankenApp.swift
+//  JaJanken
+//
+//  Created by Carlos Mbendera on 2023-04-14.
+//
+
+import SwiftUI
+
+@main
+struct JaJankenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
